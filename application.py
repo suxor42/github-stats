5404660876f788153be80dce86faa650e8de3437
suxor <suxor@ich.ms>
2016-09-05 15:48:53 +0200

    Remove ANDROID prefix

--- application.py
+++ application.py
@@ -7,8 +7,8 @@ import os
 
 OWNER = os.environ.get('GITHUB_REPO_OWNER')
 REPOSITORY = os.environ.get('GITHUB_REPO')
-ANDROID_DEV_GITHUB_NAMES = (os.environ.get('GITHUB_USERS')).split(' ')
-ANDROID_DEV_GITHUB_USERS = None
+DEV_GITHUB_NAMES = (os.environ.get('GITHUB_USERS')).split(' ')
+DEV_GITHUB_USERS = None
 
 app = Flask(__name__)
 app.config['CACHE_TYPE'] = 'simple'
@@ -17,24 +17,24 @@ app.cache = Cache(app)
 
 @app.route('/')
 def main():
-    global ANDROID_DEV_GITHUB_USERS
+    global DEV_GITHUB_USERS
     global OWNER
     global REPOSITORY
-    global ANDROID_DEV_GITHUB_NAMES
+    global DEV_GITHUB_NAMES
 
-    if ANDROID_DEV_GITHUB_USERS is None:
-        ANDROID_DEV_GITHUB_USERS = map_login_user(ANDROID_DEV_GITHUB_NAMES)
+    if DEV_GITHUB_USERS is None:
+        DEV_GITHUB_USERS = map_login_user(DEV_GITHUB_NAMES)
 
     assert OWNER is not None
     assert REPOSITORY is not None
-    assert ANDROID_DEV_GITHUB_USERS is not None
-    assert len(ANDROID_DEV_GITHUB_USERS) > 0
+    assert DEV_GITHUB_USERS is not None
+    assert len(DEV_GITHUB_USERS) > 0
 
     commits = github.get_commits(OWNER, REPOSITORY, 'master', 7)
     committer_clusters = cluster_on_date(commits, 7)
     return render_template(
         'commits.jinja2',
-        user_list=ANDROID_DEV_GITHUB_USERS,
+        user_list=DEV_GITHUB_USERS,
         commit_clusters=committer_clusters,
         owner=OWNER,
         repository=REPOSITORY
